4c48322a2cd88ce740cd270142875d16ce223ea3
lulumartic <luciamartichemes@gmail.com>
2021-12-06 10:08:57 -0300

    prueba con json2

--- js/codigoInicio.js
+++ js/codigoInicio.js
@@ -14,7 +14,7 @@ $(".buscar").on("click", function(e){
 })
 
 // url de json con propiedades 
-const URLJ =  "js/propiedades.json"
+const URLJ =  "../js/propiedades2.json"
 // renderizar todas las propiedades 
 $(document).ready( function() {
 
